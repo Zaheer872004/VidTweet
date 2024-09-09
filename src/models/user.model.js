53d8004863d71a29ebe0a195584b593702eff528
Zaheer872004 <zaheerkhan872004@gmail.com>
2024-09-09 19:12:50 +0530

    some hooks(pre) and custome mongoose methods adding

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose,{Model, Schema} from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
 
 
 const userSchema = new Schema(
@@ -41,5 +43,52 @@ const userSchema = new Schema(
     },{timestamps : true}
 )
 
+
+userSchema.pre("save", async function (next){
+
+
+    if(!this.isModified("password")) return next()
+
+    // here hashing the plantextpassword
+    const salt =  bcrypt.genSalt(process.env.SALTROUNDS);
+    const hash =  bcrypt.hash(this.password, salt);
+    this.password = hash;
+
+
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            fullName : this.fullName,
+            username : this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET, 
+        { expiresIn: ACCESS_TOKEN_EXPIRY}
+    );
+
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET, 
+        { expiresIn: REFRESH_TOKEN_EXPIRY}
+    );
+
+}
 
 export const User = mongoose.model("User",userSchema);
